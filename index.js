910297f6a09b768b5d61860523d6d651e784612b
Mark <zhrren@qq.com>
2015-12-06 21:19:12 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+ * zhrren
+ * zhrren@qq.com
+ */
+
+'use strict';
+
+module.exports = function (content, file, conf) {
+
+  //log.debug("content");
+  var result = content;
+  if (!file.isText())
+    return result;
+
+  var opts = fis.util.clone(conf);
+  if (opts.rules == null || opts.rules.length == 0)
+    return result;
+
+  for (var i = 0; i < opts.rules.length; i++) {
+    var rule = opts.rules[i];
+    result = result.replace(rule.search, rule.replace);
+  }
+
+  return result;
+};
